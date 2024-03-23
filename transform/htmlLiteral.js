fde0391dd05ef9f96334574f86a972aac34a0ff5
Alexander Orzechowski <alex@ozal.ski>
2024-03-23 16:09:59 -0400

    Reapply "transform/htmlLiteral: Drop array wrapper for children if not necessary"
    This reverts commit 3d131dc5021ca13a0cf5503ab38c49c1a19c753a.
    
    This works again

--- transform/htmlLiteral.js
+++ transform/htmlLiteral.js
@@ -43,7 +43,7 @@ const html = htm((name, props, children) => {
 	}
 
 	if (children) {
-		args.push(t.arrayExpression(children.map(child => {
+		args.push(createArray(children.map(child => {
 			if (typeof child === 'string') {
 				return t.stringLiteral(child);
 			} else {
