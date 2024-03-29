b5ff1c599a9fbc8f4d276363d5fa33ce4a2c25dc
Alexander Orzechowski <alex@ozal.ski>
2024-03-29 04:34:26 -0400

    examples/elements: put some garbage around the demo
    This garbage is meant to test the optimization in the next commit

--- examples/elements.js
+++ examples/elements.js
@@ -1,5 +1,11 @@
-import {mount, Observer} from '/index.js';
+import {mount,  Observer, html} from '/index.js';
 
 let elements = ['one', 'two', 'three'].map(name => document.createTextNode(name));
 
-mount(document.body, Observer.timer(500).map(i => elements[i % elements.length]));
+mount(document.body, html`
+	<div>
+		<div> Before switcher </div>
+		${Observer.timer(500).map(i => elements[i % elements.length])}
+		<div> After switcher </div>
+	</div>
+`);
