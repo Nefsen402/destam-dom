dc463ba4ff4593772e5d96c893b6e627688f5ed3
Alexander Orzechowski <alex@ozal.ski>
2024-04-14 16:25:19 -0400

    examples: add svg example

---
+++ examples/svg.jsx
@@ -0,0 +1,13 @@
+import {h, mount} from '/index.js';
+import htm from '/htm.js';
+
+const svg = (name, prop, ...children) => {
+	name = document.createElementNS("http://www.w3.org/2000/svg", name);
+	return h(name, prop, ...children);
+};
+
+mount(document.body,
+	<svg:svg viewBox="0 0 100 100">
+		<svg:circle cx="50" cy="50" r="50" />
+	</svg:svg>
+);
