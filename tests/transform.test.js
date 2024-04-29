9413863fff0e5af74d49c6790782a3f4bfb67a7b
Alexander Orzechowski <alex@ozal.ski>
2024-04-28 23:40:40 -0400

    tests: Add unit tests for transform/staticMount

---
+++ tests/transform.test.js
@@ -0,0 +1,64 @@
+import fs from 'node:fs';
+import vm from 'node:vm';
+import {describe, it} from 'node:test';
+import {transformBabelAST} from '../transform/staticMount.js';
+import parser from '@babel/parser';
+import t from '@babel/types';
+import generate from '@babel/generator';
+
+const files = fs.readdirSync('./tests');
+
+for (const file of files) {
+	if (file === 'transform.test.js') continue;
+	if (!file.endsWith('test.js')) continue;
+
+	describe("transform " + file, async () => {
+		let source = fs.readFileSync('./tests/' + file).toString();
+		const ast = parser.parse(source, {sourceType: 'module', code: false, ast: true});
+
+		transformBabelAST(ast);
+
+		const context = {};
+
+		// replace imports
+		for (let i = 0; i < ast.program.body.length; i++) {
+			const child = ast.program.body[i];
+			if (child.type === 'ImportDeclaration') {
+				ast.program.body.splice(i--, 1);
+
+				const mod = await import(child.source.value);
+				for (const spec of child.specifiers) {
+					if (!spec.imported) {
+						context[spec.local.name] = mod.default;
+					} else {
+						context[spec.local.name] = mod[spec.imported.name];
+					}
+				}
+			}
+		}
+
+		const code = generate.default(ast, {}, source).code;
+
+		for (let o in global) {
+			if (!(o in context)) context[o] = global[o];
+		}
+
+		if (context.test) context.test = (name, impl) => {
+			it(name, () => {
+				try {
+					return impl();
+				} catch (e) {
+					console.log(code.split('\n').map((line, num) => (num + 1) + " " + line).join("\n"));
+					throw e;
+				}
+			});
+		};
+
+		const script = new vm.Script(code, {
+			filename: file,
+		});
+
+		vm.createContext(context);
+		script.runInContext(context);
+	});
+}
