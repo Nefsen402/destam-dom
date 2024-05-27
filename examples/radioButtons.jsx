e6ebb6f11d5014901effdf574ba00d6ba5a60d62
Alexander Orzechowski <alex@ozal.ski>
2024-05-27 01:12:17 -0400

    examples/radioButtons: Add new example

---
+++ examples/radioButtons.jsx
@@ -0,0 +1,20 @@
+import {h, mount, Observer} from '/index.js';
+
+const RadioButton = ({value, name}) => {
+	return <>
+		<input name={name} type="radio" $checked={value} $onclick={() => value.set(true)} />
+		<label for={name}>{name}</label>
+		<br/>
+	</>;
+};
+
+const selected = Observer.mutable(null);
+const selector = selected.selector();
+
+const List = ({each}) => {
+	return <RadioButton value={selector(each)} name={each} />;
+};
+
+mount(document.body, <>
+	<List each={Array.from(Array(10), (_, i) => i)} />
+</>);
