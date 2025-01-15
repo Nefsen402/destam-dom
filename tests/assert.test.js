1307f0c4308ee15016ccb6867d16b720721300fc
Alexander Orzechowski <alex@ozal.ski>
2025-01-14 23:16:09 -0500

    tests/assert: Factor try catch

--- tests/assert.test.js
+++ tests/assert.test.js
@@ -1,80 +1,55 @@
-import {test} from 'node:test';
+import {test as nodeTest} from 'node:test';
 import assert from 'node:assert';
 import './document.js';
 
 import {mount, h, OArray} from '../index.js';
 
-test("assert multiple mount", () => {
+const test = (name, cb) => nodeTest(name, () => {
 	let throwed = false;
 	try {
-		const comp = h('div');
-		const elem = document.createElement("body");
-
-		mount(elem, [comp, comp]);
+		cb();
 	} catch (e) {
 		throwed = true;
 	}
-
 	assert(throwed);
+});
+
+test("assert multiple mount", () => {
+	const comp = h('div');
+	const elem = document.createElement("body");
+
+	mount(elem, [comp, comp]);
 });
 
 test("assert multiple mount dynamic", () => {
-	let throwed = false;
-	try {
-		const Component = () => {
-			return null;
-		};
+	const Component = () => {
+		return null;
+	};
 
-		const comp = h('div', {}, h(Component));
-		const elem = document.createElement("body");
+	const comp = h('div', {}, h(Component));
+	const elem = document.createElement("body");
 
-		mount(elem, [comp, comp]);
-	} catch (e) {
-		throwed = true;
-	}
-
-	assert(throwed);
+	mount(elem, [comp, comp]);
 });
 
 test("mount primitive on null mount", () => {
-	let throwed = false;
-		try {
-		const arr = OArray(['a', 'b']);
+	const arr = OArray(['a', 'b']);
 
-		mount(null, arr);
-	} catch (e) {
-		throwed = true;
-	}
-
-	assert(throwed);
+	mount(null, arr);
 });
 
 test("move element on null mount", () => {
-	let throwed = false;
-	try {
-		const arr = OArray([h('div'), h('div')]);
+	const arr = OArray([h('div'), h('div')]);
 
-		mount(null, arr);
+	mount(null, arr);
 
-		arr.splice(0, 2, arr[1], arr[0]);
-	} catch (e) {
-		throwed = true;
-	}
-
-	assert(throwed);
+	arr.splice(0, 2, arr[1], arr[0]);
 });
 
 test("move element on null mount", () => {
-	let throwed = false;
-	try {
-		const arr = Observer.mutable([h('div'), h('div')]);
+	const arr = Observer.mutable([h('div'), h('div')]);
 
-		mount(null, arr);
+	mount(null, arr);
 
-		arr.set([arr.get()[1], arr.get()[0]]);
-	} catch (e) {
-		throwed = true;
-	}
-
-	assert(throwed);
+	arr.set([arr.get()[1], arr.get()[0]]);
 });
