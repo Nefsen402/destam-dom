55c37c2e6fe37bdb11ba68633d7b6499d92fff95
Alexander Orzechowski <alex@ozal.ski>
2025-02-23 15:39:17 -0500

    tests: Rework context tests
    Tests more edge cases

--- tests/baseline.context.test.js
+++ tests/baseline.context.test.js
@@ -10,29 +10,50 @@ const test = (name, cb) => {
 
 		let failed = false;
 
-		const Comp = () => {
-			return (elem, item, before, c) => (c !== context && (failed = true), () => {})
-		};
+		const checker = (elem, item, before, c) => (c !== context && (failed = true), () => {});
 
-		mount(document.dummy, cb(Comp), undefined, context);
+		mount(document.dummy, cb(checker), undefined, context);
 
 		assert(!failed);
 	});
 };
 
-test('context', Comp => {
+test('context', comp => {
+	return comp;
+});
+
+test('context nested', comp => {
+	return h('div', {}, comp);
+});
+
+test('context nested twice', comp => {
+	return h('div', {}, h('div', {}, comp));
+});
+
+test('context array', comp => {
+	return [comp, comp];
+});
+
+test('context nested arrays', comp => {
+	return [
+		[comp, comp],
+		[comp, comp],
+	];
+});
+
+test('context component', comp => {
+	const Comp = () => {
+		return comp;
+	};
+
 	return h(Comp);
 });
 
-test('context nested', Comp => {
-	return h('div', {}, h(Comp));
-});
+test('context nested components', comp => {
+	const Comp = () => {
+		return comp;
+	};
 
-test('context nested twice', Comp => {
-	return h('div', {}, h('div', {}, h(Comp)));
-});
-
-test('context nested components', Comp => {
 	const Comp2 = () => {
 		return h(Comp);
 	};
@@ -40,7 +61,19 @@ test('context nested components', Comp => {
 	return h(Comp2);
 });
 
-test('context nested components array', Comp => {
+test('context component array', comp => {
+	const Comp2 = () => {
+		return [comp, comp, comp];
+	};
+
+	return h(Comp2);
+});
+
+test('context nested components array', comp => {
+	const Comp = () => {
+		return comp;
+	};
+
 	const Comp2 = () => {
 		return [h(Comp), h(Comp), h(Comp)];
 	};
@@ -48,34 +81,10 @@ test('context nested components array', Comp => {
 	return h(Comp2);
 });
 
-test('context nested components array and div', Comp => {
+test('context component array and div', comp => {
 	const Comp2 = () => {
-		return [h('div', {}, h(Comp)), h('div', {}, h(Comp)), h('div', {}, h(Comp))];
+		return [h('div', {}, comp), h('div', {}, comp), h('div', {}, comp)];
 	};
 
 	return h(Comp2);
-});
-
-test('context nested arrays', Comp => {
-	return [
-		[h(Comp), h(Comp)],
-		[h(Comp), h(Comp)],
-	];
-});
-
-nodetest('context array', () => {
-	const context = Symbol();
-
-	let failed = false;
-
-	const Comp = () => {
-		return [
-			(elem, item, before, c) => (c !== context && (failed = true), () => {}),
-			(elem, item, before, c) => (c !== context && (failed = true), () => {}),
-		];
-	};
-
-	mount(document.dummy, h(Comp), undefined, context);
-
-	assert(!failed);
 });
